26bc591ebe99d54d2a48cc7f589351145f3d8841
LeeJongHooni <donggle94@icloud.com>
2021-12-01 20:14:56 +0900

    회원가입 dao , dto 만듬

---
+++ src/main/java/com/semi/model/SignDAO.java
@@ -0,0 +1,68 @@
+package com.semi.model;
+
+import java.sql.*;
+import java.util.*;
+
+import com.db.conn.OracleConnect;
+
+public class SignDAO {
+	
+	OracleConnect oc = null;
+	
+	public SignDAO() {
+		oc = new OracleConnect(true);
+	}
+	
+	String query;
+	
+	public boolean insert(SignDTO dto) {
+		query = "INSERT INTO SIGNUP VALUES(SIGNUP_SEQ,'" + dto.getPkid() + "' ,"
+				+ " '" + dto.getUserid() + "', '" 
+				+ dto.getPassword() + "', '" 
+				+ dto.getUsername() + "', '"
+				+ dto.getPhoneNumber() + "', '"
+				+ dto.getEmail() + "'" +") ";
+		
+		int res = oc.insert(query);
+		if(res == 1) {
+			return true;
+		}else {
+			return false;
+		}
+		
+	}
+	public List<SignDTO> select(String userid) {
+		query = "SELECT * FROM SIGNUP WHERE USERID = '" + userid + "'";
+		
+		ResultSet rs = oc.select(userid);
+		
+		List<SignDTO> datas = new ArrayList<SignDTO>();
+		
+		try {
+			while(rs.next()) {
+				SignDTO dto = new SignDTO();
+				dto.setPkid(rs.getInt("PKID"));
+				dto.setUserid(rs.getString("USERID"));
+				dto.setPassword(rs.getString("PASSWORD"));
+				dto.setUsername(rs.getString("USERNAME"));
+				dto.setPhoneNumber(rs.getString("PHONENUMBER"));
+				dto.setEmail(rs.getString("EMAIL"));
+				datas.add(dto);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return datas;
+	}
+	
+	public void commit() {
+		oc.commit();
+	}
+	public void rollback() {
+		oc.rollback();
+	}
+	public void close() {
+		oc.close();
+	}
+
+}
---
+++ src/main/java/com/semi/model/SignDTO.java
@@ -0,0 +1,54 @@
+package com.semi.model;
+
+public class SignDTO {
+	
+	private int pkid;
+	private String userid;
+	private String password;
+	private String username;
+	private String phoneNumber;
+	private String email;
+	
+	public SignDTO() {}
+	
+	public int getPkid() {
+		return pkid;
+	}
+	public void setPkid(int pkid) {
+		this.pkid = pkid;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
+
+
