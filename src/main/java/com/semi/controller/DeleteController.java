75d6f41d199d9c18d5181f63a7f4fc21ef8f8e9f
JangJaeWan <newmy526@naver.com>
2021-12-08 21:19:36 +0900

    상세페이지 게시글 삭제 구현중

--- src/main/java/com/semi/controller/DeleteController.java
+++ src/main/java/com/semi/controller/DeleteController.java
@@ -1,11 +1,16 @@
 package com.semi.controller;
 
 import java.io.IOException;
-import javax.servlet.ServletException;
+import java.util.*;
+
+import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.semi.model.*;
+import com.semi.writer.model.*;
 
 @WebServlet("/delete")
 public class DeleteController extends HttpServlet {
@@ -13,11 +18,31 @@ public class DeleteController extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
+		String view = "WEB-INF/jsp/delete/delete.jsp";
+		RequestDispatcher rd = request.getRequestDispatcher(view);
+		rd.forward(request, response);
+				
 	}
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
+			// 게시글 번호
+			String id = request.getParameter("id");
+		
+			WriterDTO dto = new WriterDTO();
+			WriterService service = new WriterService();
+			List<WriterDTO> deleteWriter = service.select(id);
+			
+				if(service.delete(dto)) {
+					// 삭제 완료 페이지 띄움
+					String view = "/WEB-INF/jsp/delete/delete.jsp";
+					RequestDispatcher rd = request.getRequestDispatcher(view);
+					rd.forward(request, response);
+				} else {
+					// 삭제 실패
+					System.out.println("오류");
+				}
+			}
 	}
 
-}
